bb212027110755b773827e162b558e1823254921
Gisele <giskey11@gmail.com>
2020-12-09 23:04:52 -0600

    navbar component added

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 
+import {Route, Switch} from "react-router-dom"
+
 
 
 function App() {
   return (
     <>
-    <Home/>
-    <Rooms/>
-    <SingleRoom/>
-    <Error/>
+    <switch>
+
+    
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/rooms/" component={Rooms}/>
+    <Route exact path="/rooms/:slug" component={SingleRoom}/>
+    <Route component={error}/>
+    </switch>
     </>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                Hello from Navbar
+            </div>
+        )
+    }
+}
