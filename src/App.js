1abf3815e96b769152217f00376d81457faa4ae9
Gisele <giskey11@gmail.com>
2020-12-09 22:39:15 -0600

    added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React from "react";
+import "./App.css";
 
-// import './App.css';
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
+
 
 function App() {
   return (
-    <>Hello from App</>
+    <>
+    <Home/>
+    <Rooms/>
+    <SingleRoom/>
+    <Error/>
+    </>
   );
 }
 
---
+++ src/pages/Rooms.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Rooms extends Component {
+    render() {
+        return (
+            <div>
+                Hello from Room
+            </div>
+        )
+    }
+}
