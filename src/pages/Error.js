6318160b3ad762cf70b8519d346a5cfe334ea7ec
Gisele <giskey11@gmail.com>
2020-12-15 23:59:03 -0600

    Rooms

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
-import {Link} from "react-router-dom"
-
+import { Link } from "react-router-dom";
 
 export default function Error() {
-    return (
+  return (
     <Hero>
-        <Banner title="404" subtitle="page not found">
+      <Banner title="404" subtitle="page not found">
         <Link to="/" className="btn-primary">
-            <Link/>
-
-        </Banner>
+            return home
+        </Link>
+      </Banner>
     </Hero>
-    );
+  );
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,16 @@
 import React from "react"
 import Hero from "../components/Hero"
+import Banner from "../components/Hero"
+import {Link} from "react-router-dom"
 
 
+const Rooms =()=>{
+    return<Hero hero="roomsHero">
+        
+    </Hero>
+}
 
-export default function Rooms() {
-    return <Hero hero="roomsHero"/>
-};
+// export default function Rooms() {
+//     return <Hero hero="roomsHero"/>
+// };
 
