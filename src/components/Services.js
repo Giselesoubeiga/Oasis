3b3e9382077a95d1e822b28e1f133c21eff1e08b
Gisele <giskey11@gmail.com>
2020-12-16 20:03:00 -0600

    services icons and informations added

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {
+    FaCocktail,
+    FaHiking,
+    FaShuttleVan,
+    FaSwimmingPool
+} from "react-icons/fa";
 import Title from "./Title"
 
 export default class Services extends Component {
+    state={
+        services:[
+            {
+                icon:<FaCocktail/>,
+                title:"free cocktails",
+                info:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            },
+            {
+                icon:<FaHiking/>,
+                title:"Endless Hiking",
+                info:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            },
+            {
+                icon:<FaShuttleVan/>,
+                title:"Free Shuttle",
+                info:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            },
+            {
+                icon:<FaSwimmingPool/>,
+                title:"Beautiful Pool",
+                info:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            },
+        ]
+    }
     render() {
         return (
-            <div>
+            <section className="services">
             <Title title="services"/>
-            </div>
+        <div className="services-center">
+            {this.state.services.map((item,index)=>{
+                return<article key={index} className="services">
+                    <span>{item.icon}</span>
+            <h6>{item.title}</h6>
+            <p>{item.info}</p>
+                </article>
+
+            })}
+
+        </div>
+            </section>
         )
     }
 }
