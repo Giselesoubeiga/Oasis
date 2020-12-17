9ccd7545a5f864cad8299cb8c33cf56f60235bf0
Gisele <giskey11@gmail.com>
2020-12-16 19:14:57 -0600

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React from "react";
 import "./App.css";
-
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 
-import {Route, Switch} from "react-router-dom"
-import Navbar from "./components/Navbar"
 
-
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <>
-    <Navbar/>
-    <switch>
-
-    
-    <Route exact path="/" component={Home}/>
-    <Route exact path="/rooms/" component={Rooms}/>
-    <Route exact path="/rooms/:slug" component={SingleRoom}/>
-    <Route component={Error}/>
-    </switch>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            {/* <ScrollToTop /> */}
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/rooms/" component={Rooms} />
+                <Route exact path="/rooms/:slug" component={SingleRoom} />
+                <Route component={Error} />
+            </Switch>
+        </>
+    );
 }
 
 export default App;
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,16 +1,21 @@
-import React from "react"
-import Hero from "../components/Hero"
-import Banner from "../components/Hero"
-import {Link} from "react-router-dom"
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 
-const Rooms =()=>{
-    return<Hero hero="roomsHero">
-        
-    </Hero>
+export default function Rooms() {
+    return (
+        <>
+            <Hero hero="roomsHero">
+                <Banner title="Our rooms">
+                    <Link to="/" className="btn-primary">
+                        Home{" "}
+                    </Link>
+                </Banner>
+            </Hero>
+            
+            
+        </>
+    );
 }
-
-// export default function Rooms() {
-//     return <Hero hero="roomsHero"/>
-// };
-
