73ffcae866a06c611e1f2e6817b21e7b10670a5a
Gisele <giskey11@gmail.com>
2020-12-21 23:25:18 -0600

    context.js work in progress

--- src/Context.js
+++ src/Context.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
+import items from "./data" 
 
 const RoomContext = React.createContext();
 //RoomContext.Provider value={"hello"}
 
 class RoomProvider extends Component {
   state = {
+      rooms:[],
+      sorteredRoom:[],
+      loading:true
 };
+//GetData
+
+componentDidMount(){
+    //this.get Data
+    let rooms = this.formatData(items);
+  
+}
+    // formatting from data.js
+    formatData(items) {
+        let tempItems = items.map(item => {
+            let id = item.sys.id;
+            let images = item.fields.images.map(image => image.fields.file.url);
+            let room = { ...item.fields, images, id };
+            return room;
+        });
+        return tempItems;
+    }
+    
+
+
+
+
+
+
+
   render() {
     return (
       <RoomContext.Provider value="hello">
