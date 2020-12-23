5ac058347f62f3986e21fbeb4c26acf18ad610dd
Gisele <giskey11@gmail.com>
2020-12-22 21:06:21 -0600

    futuredRoom work in progress

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
-import {RoomContext} from "../Context";
+import React, { Component } from "react";
+import { RoomContext } from "../Context";
+import Title from "./Title";
+import Room from "./Room";
+
 
 export default class FeaturedRooms extends Component {
-    static contextType = RoomContext
-    render() {
-       
-        return <div> from Futured rooms</div>
-        
-    }
+  static contextType = RoomContext;
+  render() {
+    let { loading, featuredRooms: rooms } = this.context;
+    // loop through each featured rooms and return <Room> component with the list of featured rooms
+    rooms = rooms.map(room => {
+        return <Room key={room.id} room={room} />;
+    });
+    return (
+      <section className="featured-rooms">
+           <Title title="Featured Rooms" />
+           <div className="featured-rooms-center">
+                    {/* if loading = true, set loading component (has loading
+                    screen) : or render the rooms */}
+                    {loading ? <Loading /> : rooms}
+                </div>
+
+
+        <Loading />
+      </section>
+    );
+  }
 }
