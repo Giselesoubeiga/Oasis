394fbe468661e730d6b866151380079696c686ff
Gisele <giskey11@gmail.com>
2020-12-29 23:23:11 -0600

    fixed typos error

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,58 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import defaultBcg from "../images/room-1.jpeg";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
-import {Link} from "react-router-dom";
-import {RoomContext} from "../context"
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
 
 export default class SingleRoom extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            slug: this.props.match.params.slug,
-            defaultBcg: defaultBcg
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg: defaultBcg,
+    };
+  }
+
+  static contextType = RoomContext;
+  // componentDidMount(){
+
+  // }
+  render() {
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3> no such room could be found...</h3>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </div>
+      );
     }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images,
+    } = room;
 
-static contextType= RoomContext;
-    // componentDidMount(){
-
-    // }
-    render() {
-        const { getRoom } = this.context;
-        const room = getRoom(this.state.slug);
-        if (!room) {
-            return (
-              <div className="error">
-                <h3> no such room could be found...</h3>
-                <Link to="/rooms" className="btn-primary">
-                  back to rooms
-                </Link>
-              </div>
-            );
-          }
-        const {
-            name,
-            description,
-            capacity,
-            size,
-            price,
-            extras,
-            breakfast,
-            pets,
-            images
-        } = room;
-
-        
-        return 
-           <Hero hero="roomsHero">
-               <Banner title={`${name} room`}>
-                   <Link to="/rooms" className="btn-primary">
-                       back to room
-                   </Link>
-
-               </Banner>
-
-           </Hero>
-    }
+    return(
+    <Hero hero="roomsHero">
+      <Banner title={`${name} room`}>
+        <Link to="/rooms" className="btn-primary">
+          back to room
+        </Link>
+      </Banner>
+    </Hero>
+    );
+  }
 }
