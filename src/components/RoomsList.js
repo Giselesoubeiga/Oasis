b267b1816a47cce2fb5a1bab675979bf7d3caa17
Gisele <giskey11@gmail.com>
2021-01-06 23:05:25 -0600

    fixed typos

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function RoomList() {
+export default function RoomsList() {
     return (
         <div>
             Hello from RoomList
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
+import RoomsContainer from "../components/RoomContainer"
 
 export default function Rooms() {
   return (
