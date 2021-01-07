e7da4d7f5e91f70d85e4ed1f4414175bd2f039c4
Gisele <giskey11@gmail.com>
2021-01-06 23:30:40 -0600

    fixed errors

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import RoomsFilter from "./RoomFilter"
 import RoomsList from "./RoomsList"
 
-export default function RoomContainer() {
+export default function RoomsContainer() {
     return (
         <div>
             Hello from Container
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
-import RoomsContainer from "../components/RoomContainer"
+import RoomsContainer from "../components/RoomsContainer"
 
 export default function Rooms() {
   return (
@@ -14,6 +14,7 @@ export default function Rooms() {
           </Link>
         </Banner>
       </Hero>
+      <RoomsContainer/>
     </>
   );
 }
