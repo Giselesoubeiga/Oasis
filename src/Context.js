8b3c450534d75082119d1810d5549e3bf86ef753
Gisele <giskey11@gmail.com>
2021-01-08 00:00:24 -0600

    work in progress

--- src/Context.js
+++ src/Context.js
@@ -10,18 +10,41 @@ class RoomProvider extends Component {
     sorteredRoom: [],
     featuredRooms: [],
     loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false
   };
+
+
+
   //GetData
 
   componentDidMount() {
     //this.get Data
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter(room => room.featured === true);
+    //
+
+    let maxPrice = Math.max(...rooms.map(item => item.price));
+    let maxSize = Math.max(...rooms.map(item => item.size));
+
+
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
       loading: false,
+
+      //
+      price: maxPrice,
+      maxPrice,
+      maxSize
     });
   }
   // formatting from data.js
