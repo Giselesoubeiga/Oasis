4a4035b4d1518847382a72b6569cc02b433fe544
Gisele <giskey11@gmail.com>
2021-01-08 16:21:52 -0600

    work in progress

--- src/Context.js
+++ src/Context.js
@@ -68,15 +68,37 @@ class RoomProvider extends Component {
 
   //
   handleChange = event =>{
-    const type = event.target.type
-    const name = event.target.name
-    const value = event.target.value
-    console.log(type, name, value);
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState(
+      {
+        [name]: value
+      },
+      this.filterRooms
+    );
+    
   }
 
 
   filterRooms=()=>{
-    console.log("hello");
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets
+    } = this.state;
+
+    let tempRooms = [...rooms];
+    // filter by type
+    if (type !== "all") {
+      tempRooms = tempRooms.filter(room => room.type === type);
+    }
   };
 
 
--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -3,21 +3,20 @@ import { useContext } from "react";
 import { RoomContext } from "../context";
 import Title from "./Title";
 
+// get all unique values
+
 export default function RoomFilter() {
     const context = useContext(RoomContext);
-    const {
-        handleChange,
-        type,
-        capacity,
-        price,
-        minPrice,
-        maxPrice,
-        minSize,
-        maxSize,
-        breakfast,
-        pets
-      } = context;
-    
-    return 
-    <section className="filter-container"></section>
-}
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
+  ));
