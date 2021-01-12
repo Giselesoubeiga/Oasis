1c0832155102cd043c6a055903d88ee647131f12
Gisele <giskey11@gmail.com>
2021-01-11 22:17:14 -0600

    worked on the context

--- src/Context.js
+++ src/Context.js
@@ -95,21 +95,42 @@ class RoomProvider extends Component {
     } = this.state;
 
     let tempRooms = [...rooms];
+    // transform values
+    // get capacity
+    capacity = parseInt(capacity);
+    price = parseInt(price);
     // filter by type
     if (type !== "all") {
       tempRooms = tempRooms.filter(room => room.type === type);
     }
-
     // filter by capacity
     if (capacity !== 1) {
       tempRooms = tempRooms.filter(room => room.capacity >= capacity);
     }
+
+    // filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price);
+    //filter by size
+    tempRooms = tempRooms.filter(
+      room => room.size >= minSize && room.size <= maxSize
+    );
+
+    //filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true);
+    }
+
+     //filter by pets
+     if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true);
+    }
+    this.setState({
+      sortedRooms: tempRooms
+    });
+    
   };
 
-
-
-
-  render() {
+ render() {
     return (
       <RoomContext.Provider value={{
         ...this.state, 
@@ -123,3 +144,13 @@ class RoomProvider extends Component {
 
 const RoomConsumer = RoomContext.Consumer;
 export { RoomProvider, RoomConsumer, RoomContext };
+
+export function withRoomConsumer(Component) {
+  return function ConsumerWrapper(props) {
+    return (
+      <RoomConsumer>
+        {value => <Component {...props} context={value} />}
+      </RoomConsumer>
+    );
+  };
+}
